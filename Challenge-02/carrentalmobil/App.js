b3ec35dad78999ab1dda39e512dce5555428cf26
kevinsiregar22 <siregar25v@gmail.com>
2022-03-11 13:41:10 +0700

    splash screen

---
+++ Challenge-02/carrentalmobil/App.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React, {useEffect} from 'react'
+import SplashScreen from 'react-native-splash-screen'
+
+
+export default function App() {
+
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+  
+  return (
+    <View>
+      <Text>Car Rental Mobil</Text>
+    </View>
+  )
+}
+
